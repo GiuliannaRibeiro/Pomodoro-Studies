af7489efa0973aba13ece283154cec141ae00edc
Giulianna Ribeiro <giuliannaribeiro00@gmail.com>
2023-05-12 11:20:25 -0300

    Update App.tsx

--- src/Pages/App.tsx
+++ src/Pages/App.tsx
@@ -6,17 +6,13 @@ import Cronometro from '../components/Cronometro';
 import { Itarefa } from '../types/tarefa';
 
 function App() {
-  const [tarefas, setTarefas] = useState<Itarefa[] | []>([]); //pode ser tanto um array de Itarefa quanto um array vazio
-  //criar um estado de selecionado, para o cronometor não precisar ter essa responsabilidade:
+  const [tarefas, setTarefas] = useState<Itarefa[] | []>([]); 
   const [selecionado, setSelecionado] = useState<Itarefa>();
-  //criar uma função em que vamos iterar sobre o tarefas e colocaremos o item selecionado dentro de selecionado.
+    
   function selecionaTarefa(tarefaSelecionada: Itarefa) {
     setSelecionado(tarefaSelecionada);
-    //estamos pegando o estado antigo dentro do setTarefas e dando um map nele, para ver se a tarefa que estamos iterando é a tarefaselecionada ou não
     setTarefas(tarefasAnteriores => tarefasAnteriores.map(tarefa => ({
       ...tarefa,
-      //se a tarefa que estamos iterando é igual ao id da tarefa selecionada, isso significa que ela vai ser a tarefa selecionada
-      //? se for true retorna true : e se não for retorna false 
       selecionado: tarefa.id === tarefaSelecionada.id ? true : false
     })))
   }
