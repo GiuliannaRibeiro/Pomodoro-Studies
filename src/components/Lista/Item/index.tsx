0b39e0b1ef336302b920df30b3db467ef899ca64
Giulianna Ribeiro <giuliannaribeiro00@gmail.com>
2023-05-12 11:18:02 -0300

    Update index.tsx

--- src/components/Lista/Item/index.tsx
+++ src/components/Lista/Item/index.tsx
@@ -5,7 +5,7 @@ interface Props extends Itarefa {
   selecionaTarefa: (tarefaSelecionada: Itarefa) => void;
 }
 
-export default function item({ //conseguimos exportar a função assim que criamos ela, se não quiser também pode exportar lá em baixo que não faz diferença
+export default function item({ 
   tarefa,
   tempo,
   selecionado,
@@ -16,7 +16,7 @@ export default function item({ //conseguimos exportar a função assim que criam
   return (
     <li
       className={`${style.item} ${selecionado ? style.itemSelecionado : ''}
-      ${completado ? style.itemCompletado : ''}`} //condicional do itemselecionado: se ele estiver selecionado retorna tyle.itemSelecionado, e se não estiver retorna nada, uma string vazia
+      ${completado ? style.itemCompletado : ''}`}
       onClick={() =>
         !completado && selecionaTarefa(
         {
